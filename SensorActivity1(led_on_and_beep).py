284dcce9d226a481d1520a3cc2cd94e737ff62a2
Rohan Pankaj <rohansp01@gmail.com>
2019-05-29 10:47:55 -0700

    Added sensor activity one which is turning on led lights and beeping

---
+++ SensorActivity1(led_on_and_beep).py
@@ -0,0 +1,21 @@
+
+#-------------Setup----------------
+
+import Ed
+
+Ed.EdisonVersion = Ed.V2
+
+Ed.DistanceUnits = Ed.INCH
+Ed.Tempo = Ed.TEMPO_MEDIUM
+
+#--------Your code below-----------
+
+Ed.LeftLed(Ed.ON)
+Ed.PlayMyBeep(8000)
+Ed.LeftLed(Ed.OFF)
+Ed.PlayMyBeep(8000)
+
+Ed.RightLed(Ed.ON)
+Ed.PlayMyBeep(8000)
+Ed.RightLed(Ed.OFF)
+Ed.PlayMyBeep(8000)
