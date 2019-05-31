3a8ce9948ae9bc70d540e6e7c86b00d379fe6dae
Rohan Pankaj <rohansp01@gmail.com>
2019-05-31 10:08:19 -0700

    Made Task2 in U2-2.1

---
+++ MakeEdisonBlink.py
@@ -0,0 +1,22 @@
+
+#-------------Setup----------------
+
+import Ed
+
+Ed.EdisonVersion = Ed.V2
+
+Ed.DistanceUnits = Ed.INCH
+Ed.Tempo = Ed.TEMPO_MEDIUM
+
+#--------Your code below-----------
+
+#Turn Lights On
+Ed.LeftLed(Ed.ON)
+Ed.RightLed(Ed.ON)
+
+#Turn lights off
+Ed.LeftLed(Ed.OFF)
+Ed.RightLed(Ed.OFF)
+
+
+
